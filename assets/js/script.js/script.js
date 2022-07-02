3fd6643c460ac8d07688e816284716bbf53b553d
Oscar Acevedo <oacevedovilches@gmail.com>
2022-07-02 16:25:09 -0400

    color language settings

--- assets/js/script.js/script.js
+++ assets/js/script.js/script.js
@@ -1,9 +1,9 @@
 document.getElementById("btn-1").addEventListener("click", function(){
 
-    var cantidad = document.querySelector('#input-1').value; 
+    var quantity = document.querySelector('#input-1').value; 
     var color = document.querySelector('#input-2').value; 
-    var precio = document.querySelector('#input-3').value;           
-    document.querySelector('.tot').innerHTML=cantidad*precio;
-    document.querySelector('.cant').innerHTML=cantidad;
+    var price = document.querySelector('#input-3').value;           
+    document.querySelector('.tot').innerHTML=quantity*price;
+    document.querySelector('.cant').innerHTML=quantity;
     test.setAttribute("style", `background-color:${color}`).innerHTML;
     });
